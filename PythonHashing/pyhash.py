e4d8ef654fdcb15afd9b4ebe0f4a5188e24476ad
Shail Shouryya <sshouryy@ucsc.edu>
2019-04-13 00:06:11 -0700

    created the script exactly as outlined in article

---
+++ PythonHashing/pyhash.py
@@ -0,0 +1,19 @@
+import os
+import hashlib
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+args = parser.parse_args()
+
+md5 = hashlib.md5()
+sha1 = hashlib.sha1()
+
+try:
+    with open(args.file, 'rb') as f:
+        buf = f.read()
+        md5hasher.update(buf)
+        sha1hasher.update(buf)
+    print("{} {} {}".format(os.path.basename(args.file), md5.hexdigest(), sha1.hexdigest())
+except FileNotFoundError as e:
+    print(e)
