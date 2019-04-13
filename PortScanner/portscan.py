530a427a8b674c99e930ba8e19562aecaa50f58b
Shail Shouryya <sshouryy@ucsc.edu>
2019-04-12 21:50:18 -0700

    created script as indicated in article

---
+++ PortScanner/portscan.py
@@ -0,0 +1,24 @@
+import socket
+import argparse
+import sys
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument('host')
+args = parser.parse_args()
+
+t1 = datetime.now()
+
+try:
+    for port in range(1, 1025):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((args.host, port))
+        if result == 0:
+            print("Port: {} Open".format(port))
+        sock.close()
+except KeyboardInterrupt:
+    sys.exit()
+    
+t2 = datetime.now()
+print("Scanning completed in: {}".format(t2-t1))
