3076de62d799d9e554934cab416709ba2c09e173
Shail Shouryya <sshouryy@ucsc.edu>
2019-04-17 19:35:24 -0700

    added connexion dependency

--- 06_RESTAPIsWithFlask,Connexion,and_SQLAlchemy/server.py
+++ 06_RESTAPIsWithFlask,Connexion,and_SQLAlchemy/server.py
@@ -1,10 +1,11 @@
-from flask import (
-    Flask,
-    render_template
-)
+from flask import render_template
+import connexion
 
 # Create the application instance
-app = Flask(__name__, template_folder="templates")
+app = connexion.App(__name__, specification_dir='./')
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
 
 # Create a URL route in our application for "/"
 @app.route('/')
@@ -12,11 +13,10 @@ def home():
     """
     This function just responds to the browser ULR
     localhost:5000/
-
     :return:        the rendered template 'home.html'
     """
     return render_template('home.html')
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
