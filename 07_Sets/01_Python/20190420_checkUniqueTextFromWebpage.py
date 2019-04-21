17dabdc6a3f8298100d947985cd065055090e345
Shail Shouryya <sshouryy@ucsc.edu>
2019-04-20 19:30:55 -0700

    indicated what happens without specifying BS parse type

--- 07_Sets/01_Python/20190420_checkUniqueTextFromWebpage.py
+++ 07_Sets/01_Python/20190420_checkUniqueTextFromWebpage.py
@@ -3,7 +3,20 @@ from bs4 import BeautifulSoup
 
 url = "https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python"
 html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, 'html.parser') #leaving out 'html.parser' yielded the following output in terminal:
+'''
+C:\Users\Shail\Anaconda3\lib\site-packages\bs4\__init__.py:181: UserWarning: No parser was explicitly specified, so I'm using the best available HTML parser for this system ("lxml"). This usually isn't a problem, but if you run this code on another system, or in a different virtual environment, it may use a different parser and behave differently.
+
+The code that caused this warning is on line 6 of the file 20190420_checkUniqueTextFromWebpage.py. To get rid of this warning, change code that looks like this:
+
+ BeautifulSoup(YOUR_MARKUP})
+
+to this:
+
+ BeautifulSoup(YOUR_MARKUP, "lxml")
+
+  markup_type=markup_type))
+'''
 
 # kill all script and style elements
 for script in soup(["script", "style"]):
@@ -19,5 +32,5 @@ chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
 # drop blank lines
 text = '\n'.join(chunk for chunk in chunks if chunk)
 
-# print(text)
+print(text)
 print (len(set(text)))
