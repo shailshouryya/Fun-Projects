ea56f73ca3e08a9c8c1f380abdd8650d5a1a4c81
Shail-Shouryya <shailshouryya@gmail.com>
2019-06-03 19:05:48 -0700

    installed django

---
+++ Projects/10_Django_cfe_freeCodeCamp/Dev/trydjango/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\shail\folders\learn\fun-projects\projects\10_django_cfe_freecodecamp\dev\trydjango\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
